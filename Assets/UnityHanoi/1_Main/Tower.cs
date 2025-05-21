2e1ac6985141a7fa066326acdab45a0c42bd9022
Khiew Jian Bin <khiewbin@gmail.com>
2025-05-21 21:25:25 +0800

    update ??

--- Assets/UnityHanoi/1_Main/Tower.cs
+++ Assets/UnityHanoi/1_Main/Tower.cs
@@ -4,4 +4,9 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] GameObject baseModel;
     
+    public void SetBaseScale(int diskNum)
+    {
+        var scale = baseModel.transform.localScale;
+
+    }
 }
